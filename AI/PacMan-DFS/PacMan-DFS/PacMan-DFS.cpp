173a1e13b657109c338e952136f3a2e1a9771bac
Jazzinghen <jazzinghen@gmail.com>
2020-01-03 17:32:03 +0900

    Finally completed.
    The problem definition was absurdely written. I had to use the
    discussions not to get the solution, but to understand what they
    wanted me to do.

--- AI/PacMan-DFS/PacMan-DFS/PacMan-DFS.cpp
+++ AI/PacMan-DFS/PacMan-DFS/PacMan-DFS.cpp
@@ -4,6 +4,8 @@
 #include <unordered_set>
 #include <stack>
 
+#define PRINT_VISITED
+
 // Point definition
 // First: row
 // Second: column
@@ -20,17 +22,20 @@ struct hash_pair {
     }
 };
 
-void add_location(const Point& start, const Point& next_location, const Point& grid_size, std::stack<Point>& v_stack, std::vector<Point>& p_matrix, const std::vector<bool>& vis_matrix)
+// This adds a new location to the stack and updates all the rest of the structures,
+// visited and previous (for backtrack)
+void add_location(const Point& start, const Point& next_location, const std::vector<std::string>& grid, std::stack<Point>& v_stack, std::vector<Point>& p_matrix, std::vector<bool>& vis_matrix)
 {
-    size_t flat_pos = (grid_size.second * next_location.first) + next_location.second;
-    if (!vis_matrix[flat_pos]) {
+    size_t flat_pos = (grid[0].size() * next_location.first) + next_location.second;
+    if ((!vis_matrix[flat_pos]) && (grid[next_location.first][next_location.second] != '%')) {
         p_matrix[flat_pos] = start;
+        vis_matrix[flat_pos] = true;
+        v_stack.push({ next_location.first, next_location.second });
     }
-    v_stack.push({ next_location.first, next_location.second });
 }
 
 // Seems like a pretty good function to split
-void update_visit_stack(const Point& start, const std::vector<std::string>& grid, std::stack<Point>& v_stack, std::vector<Point>& p_matrix, const std::vector<bool> & vis_matrix)
+void update_visit_stack(const Point& start, const std::vector<std::string>& grid, std::stack<Point>& v_stack, std::vector<Point>& p_matrix, std::vector<bool> & vis_matrix)
 {
     size_t row = start.first;
     size_t col = start.second;
@@ -38,16 +43,16 @@ void update_visit_stack(const Point& start, const std::vector<std::string>& grid
     size_t max_row = grid.size();
     size_t max_col = grid[0].size();
     if (row > 0) {
-        add_location(start, { row - 1, col }, { max_row, max_col }, v_stack, p_matrix, vis_matrix);
+        add_location(start, { row - 1, col }, grid, v_stack, p_matrix, vis_matrix);
     }
     if (col > 0) {
-        add_location(start, { row, col - 1 }, { max_row, max_col }, v_stack, p_matrix, vis_matrix);
+        add_location(start, { row, col - 1 }, grid, v_stack, p_matrix, vis_matrix);
     }
     if (col < (max_col - 1)) {
-        add_location(start, { row, col + 1 }, { max_row, max_col }, v_stack, p_matrix, vis_matrix);
+        add_location(start, { row, col + 1 }, grid, v_stack, p_matrix, vis_matrix);
     }
     if (row < (max_row - 1) ) {
-        add_location(start, { row + 1, col }, { max_row, max_col }, v_stack, p_matrix, vis_matrix);
+        add_location(start, { row + 1, col }, grid, v_stack, p_matrix, vis_matrix);
     }
 }
 
@@ -71,22 +76,17 @@ void dfs(const Point& pacman_start, const Point& food_loc, const std::vector<std
     while (!visit_stack.empty() && !food_found) {
         Point current_loc = visit_stack.top();
         visit_stack.pop();
+        visit_path.push_back(current_loc);
 
-        size_t flat_pos = (col * current_loc.first) + current_loc.second;
         char current_char = grid[current_loc.first][current_loc.second];
-
         if (current_char == '.') {
-            visit_path.push_back(current_loc);
-
             food_found = true;
-        } else if ((!visited[flat_pos]) && (current_char != '%')) {
-            visited[flat_pos] = true;
-            visit_path.push_back(current_loc);
-
+        } else {
             update_visit_stack(current_loc, grid, visit_stack, precedent, visited);
         }
     }
 
+#ifdef PRINT_VISITED
     std::cout << "Visited nodes: " << std::endl;
 
     for (size_t r = 0; r < row; ++r) {
@@ -94,7 +94,8 @@ void dfs(const Point& pacman_start, const Point& food_loc, const std::vector<std
             size_t flat_pos = (col * r) + c;
             if (visited[flat_pos]) {
                 std::cout << "x";
-            } else {
+            }
+            else {
                 std::cout << grid[r][c];
             }
         }
@@ -102,13 +103,18 @@ void dfs(const Point& pacman_start, const Point& food_loc, const std::vector<std
     }
 
     std::cout << std::endl;
+#endif // PRINT_VISITED
 
+
+    // First print all the visited locations
     size_t visited_nodes = visit_path.size();
     std::cout << visited_nodes << std::endl;
     for (const auto& coords : visit_path) {
         std::cout << coords.first << " " << coords.second << std::endl;
     }
 
+
+    // Then find the path we followed to get here and add it to a stack
     std::stack<Point> travelled_path;
     travelled_path.push(food_loc);
 
@@ -120,6 +126,7 @@ void dfs(const Point& pacman_start, const Point& food_loc, const std::vector<std
         travelled_path.push(prev_loc);
     }
 
+    // Print it in reverse order
     size_t path_length = travelled_path.size() - 1;
     std::cout << path_length << std::endl;
     while (!travelled_path.empty()) {
