142cec0bad488f68b8e6a7df2d9fa3c19c88466b
Jazzinghen <jazzinghen@gmail.com>
2020-01-14 21:36:06 +0900

    Time wasted just because I forgot to build.

--- CtCI/dynamic_programming/triple_step/triple_step.cpp
+++ CtCI/dynamic_programming/triple_step/triple_step.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
-#include "triple-step.h"
+#include <vector>
+#include "triple_step.h"
 
-int main(int argc, char** argv)
+size_t triple_step(int steps)
 {
-    std::cout << "Hello World!" << std::endl;
+    std::vector<size_t> combos(steps);
+
+    combos[0] = 1;
+    combos[1] = 2;
+    combos[2] = 4;
+
+    for (int i = 3; i < steps; ++i)
+    {
+        combos[i] = combos[i - 1] + combos[i - 2] + combos[i - 3];
+    }
+
+    return combos[steps - 1];
+}
+
+int main(int argc, char **argv)
+{
+    int target = 0;
+    std::cin >> target;
+
+    size_t combinations = triple_step(target);
+    std::cout << combinations;
+
     return 0;
 }
