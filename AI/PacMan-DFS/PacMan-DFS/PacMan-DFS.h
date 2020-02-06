91346eb09b77ea23ebb926f7b2bb179af6e872b6
Edrick Wong <wong.edrick@gmail.com>
2020-02-05 23:22:40 -0800

    I fibonnaci indented everything but you don't use enough brackets to make it cool

--- AI/PacMan-DFS/PacMan-DFS/PacMan-DFS.h
+++ AI/PacMan-DFS/PacMan-DFS/PacMan-DFS.h
@@ -1,4 +1,4 @@
-﻿// PacMan-DFS.h : Include file for standard system include files,
+// PacMan-DFS.h : Include file for standard system include files,
 // or project specific include files.
 
 #pragma once
--- CtCI/dynamic_programming/triple_step/triple_step.cpp
+++ CtCI/dynamic_programming/triple_step/triple_step.cpp
@@ -4,27 +4,27 @@
 
 size_t triple_step(int steps)
 {
-    std::vector<size_t> combos(steps);
+  std::vector<size_t> combos(steps);
 
-    combos[0] = 1;
-    combos[1] = 2;
-    combos[2] = 4;
+  combos[0] = 1;
+  combos[1] = 2;
+  combos[2] = 4;
 
-    for (int i = 3; i < steps; ++i)
-    {
-        combos[i] = combos[i - 1] + combos[i - 2] + combos[i - 3];
-    }
+  for (int i = 3; i < steps; ++i)
+  {
+      combos[i] = combos[i - 1] + combos[i - 2] + combos[i - 3];
+  }
 
-    return combos[steps - 1];
+  return combos[steps - 1];
 }
 
 int main(int argc, char **argv)
 {
-    int target = 0;
-    std::cin >> target;
+  int target = 0;
+  std::cin >> target;
 
-    size_t combinations = triple_step(target);
-    std::cout << combinations;
+  size_t combinations = triple_step(target);
+  std::cout << combinations;
 
-    return 0;
+  return 0;
 }
